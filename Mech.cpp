2976d0a7ac7f60da43bdabdda76058f3ce41e475
Purpursarkans <goga.danilov10@gmail.com>
2022-05-14 17:49:24 +0400

    update

--- Mech.cpp
+++ Mech.cpp
@@ -3,7 +3,15 @@
 
 int main()
 {
-    
+    system("mode con cols=80 lines=15");
     std::cout << "hi" << std::endl;
+
+    LPPOINT p;
+    
+    while (1)
+    {
+        GetCursorPos(p);
+        std::cout << "x - " << p->x << "y - " << p->y << std::endl;
+    }
     system("pause");
 }
